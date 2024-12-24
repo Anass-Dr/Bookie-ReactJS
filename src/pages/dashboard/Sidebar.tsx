37a61a1064ccafa656f660addd1226532f96f0f2
Anass-Dr <anasdrissi77@gmail.com>
2024-12-24 11:56:49 +0100

    feat: add Sidebar component to Dashboard page

---
+++ src/pages/dashboard/Sidebar.tsx
@@ -0,0 +1,74 @@
+import { Link, useLocation } from "react-router-dom";
+import { useAuth } from "react-oidc-context";
+
+const SideBar = () => {
+  const location = useLocation();
+  const auth = useAuth();
+  const isAdmin = auth.user?.profile["cognito:groups"].includes("admin");
+
+  return (
+    <div className="account_dashboard_sidebar">
+      <div className="sidebar_widget_body d-flex account_dashboard_sidebar_profile">
+        <div className="">
+          <img src="/assets/img/profile-img.png" alt="account" />
+        </div>
+        <div className="">
+          <div className="greetings">Hello</div>
+          <div className="name">Muaz BK</div>
+        </div>
+      </div>
+      <div className="sidebar_widget_body p-0">
+        <ul className="sidebar_widget_menu">
+          <li>
+            <Link
+              className={
+                location.pathname === "/dashboard/account-info" ? "active" : ""
+              }
+              to="/dashboard/account-info"
+            >
+              My Profile
+            </Link>
+          </li>
+          <li>
+            <Link
+              className={
+                location.pathname === "/dashboard/book-loans" ? "active" : ""
+              }
+              to="/dashboard/book-loans"
+            >
+              Book loans
+            </Link>
+          </li>
+          {isAdmin && (
+            <>
+              <li>
+                <Link
+                  className={
+                    location.pathname === "/dashboard/book-list" ? "active" : ""
+                  }
+                  to="/dashboard/book-list"
+                >
+                  Book list
+                </Link>
+              </li>
+              <li>
+                <Link
+                  className={
+                    location.pathname === "/dashboard/book-create"
+                      ? "active"
+                      : ""
+                  }
+                  to="/dashboard/book-create"
+                >
+                  Add Book
+                </Link>
+              </li>
+            </>
+          )}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default SideBar;
