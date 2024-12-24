71a6e58423384c4ebf0a4d2442a62b38a2963f36
Anass-Dr <anasdrissi77@gmail.com>
2024-12-24 11:22:47 +0100

    Refactor App.tsx to include authentication provider for Cognito

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import router from "./router";
 import { RouterProvider } from "react-router-dom";
+import { AuthProvider } from "react-oidc-context";
+
+const cognitoAuthConfig = {
+  authority: import.meta.env.VITE_AUTHORITY,
+  client_id: import.meta.env.VITE_CLIENT_ID,
+  redirect_uri: import.meta.env.VITE_REDIRECT_URI,
+  response_type: "code",
+  scope: "email openid phone",
+};
+
 function App() {
   return (
     <>
-      <RouterProvider router={router} />
+      <AuthProvider {...cognitoAuthConfig}>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </>
   );
 }
