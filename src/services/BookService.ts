8c407199b4676b3059af39d30e9687a2059c6727
Anass-Dr <anasdrissi77@gmail.com>
2024-12-24 11:06:29 +0100

    Refactor BookService to include new methods for borrowing, returning, creating, updating, and deleting books

--- src/services/BookService.ts
+++ src/services/BookService.ts
@@ -1,5 +1,6 @@
 import axiosInstance from "../config/axios";
 import { toast } from "react-toastify";
+import { BookInterface } from "../interfaces/book.interface";
 
 class BookService {
   static async getAll() {
@@ -20,6 +21,16 @@ class BookService {
     }
   }
 
+  static async getBorrowed(sub) {
+    if (!sub) return;
+    try {
+      const res = await axiosInstance.get(`/books/borrowed/${sub}`);
+      return res.data;
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
+  }
+
   static async getOne(id) {
     try {
       const res = await axiosInstance.get(`/books/${id}`);
@@ -29,10 +40,56 @@ class BookService {
     }
   }
 
-  static async borrow(id) {
+  static async borrow(id, sub) {
     try {
-      const res = await axiosInstance.post(`/books/${id}/borrow`);
+      const res = await axiosInstance.post(`/books/${id}/borrow`, {
+        userId: sub,
+      });
       toast.success(res.data.message);
+      return true;
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
+  }
+
+  static async returnBook(id, sub) {
+    try {
+      await axiosInstance.post(`/books/${id}/return`, { userId: sub });
+      toast.success("Book returned successfully");
+      return true;
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    }
+  }
+
+  static async create(book: BookInterface) {
+    try {
+      await axiosInstance.post("/books", book);
+      toast.success("Book created successfully");
+      return true;
+    } catch (error) {
+      const resErrorMsgs = error?.response?.data?.message;
+      if (resErrorMsgs) resErrorMsgs.forEach((msg) => toast.error(msg));
+      else toast.error(error.message);
+    }
+  }
+
+  static async update(id, book: BookInterface) {
+    try {
+      await axiosInstance.patch(`/books/${id}`, book);
+      toast.success("Book updated successfully");
+      return true;
+    } catch (error) {
+      const resErrorMsgs = error?.response?.data?.message;
+      if (resErrorMsgs) resErrorMsgs.forEach((msg) => toast.error(msg));
+      else toast.error(error.message);
+    }
+  }
+
+  static async delete(id) {
+    try {
+      await axiosInstance.delete(`/books/${id}`);
+      toast.success("Book deleted successfully");
       return true;
     } catch (error) {
       toast.error(error?.response?.data?.message);
