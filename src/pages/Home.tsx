c14e86c64adfa21775d187c13ea0ecd5e2ac37e7
Anass-Dr <anasdrissi77@gmail.com>
2024-12-29 22:57:05 +0100

    Add cognito token in localstorage when user authenticated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,22 @@
+import { useEffect } from "react";
+import { useAuth } from "react-oidc-context";
 import BestSelling from "../components/home/BestSelling";
 import FeatureAuthors from "../components/home/FeatureAuthors";
 import Hero from "../components/home/Hero";
 import NewProducts from "../components/home/NewProducts";
 
 const Home = () => {
+  const auth = useAuth();
+
+  useEffect(() => {
+    if (
+      auth.isAuthenticated &&
+      localStorage.getItem("biblio-ticket") === null
+    ) {
+      localStorage.setItem("biblio-ticket", auth.user?.id_token || "");
+    }
+  }, [auth]);
+
   return (
     <>
       <Hero />
